6d134764529b35ad87f28d075595daf4d25cc8d7
tomaslcarballo <tomi2514@gmail.com>
2021-05-14 16:37:02 -0300

    Hago calcularValor abstracto y elimino metodos innecesarios

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -7,6 +7,7 @@ abstract public class Movimiento {
   private double monto;
 
   abstract boolean isDeposito();
+  abstract double calcularValor(Cuenta cuenta);
 
   public Movimiento(LocalDate fecha, double monto) {
     this.fecha = fecha;
@@ -21,52 +22,14 @@ abstract public class Movimiento {
     return fecha;
   }
 
-  /*
-    public boolean fueDepositado(LocalDate fecha) {
-      return isDeposito() && esDeLaFecha(fecha);
-    }
-
-    public boolean fueExtraido(LocalDate fecha) {
-      return isExtraccion() && esDeLaFecha(fecha);
-    }
-
-    public boolean esDeLaFecha(LocalDate fecha) {
-      return this.fecha.equals(fecha);
-    }
-
-    public boolean isDeposito() {
-      return esDeposito;
-    }
-
-    public boolean isExtraccion() {
-      return !esDeposito;
-    }
-
-  public void agregateA(Cuenta cuenta) {
-    cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto);
-    //Esto quizas puedo hacerlo directamente en la cuenta
-
-  }
-
-  public double calcularValor(Cuenta cuenta) {
-    if (esDeposito) {
-      return cuenta.getSaldo() + getMonto();
-    } else {
-      return cuenta.getSaldo() - getMonto();
-      //En vez de poner deposito true puedo hacer una subclase de Movimiento y que cada una calcule el valor
-
-    }
-  }
 }
-*/
 
-}
 
  class Deposito extends Movimiento{
   public Deposito(LocalDate fecha, double monto) {
     super(fecha, monto);
   }
+
   public boolean isDeposito() { return true;}
 
   public double calcularValor(Cuenta cuenta){
